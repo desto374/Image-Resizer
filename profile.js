8826b72c21cb85c93f4eadc83c630853b701941b
desto374 <156571181+desto374@users.noreply.github.com>
2026-01-07 00:11:54 -0800

    Add files via upload

--- profile.js
+++ profile.js
@@ -2,7 +2,7 @@ const IS_LOCAL =
   location.hostname === "localhost" || location.hostname === "127.0.0.1";
 const API_BASE = IS_LOCAL
   ? "http://127.0.0.1:8000"
-  : "https://image-resizer-deao.onrender.com";
+  : "https://image-resizer-rxnr.onrender.com";
 
 const userPanel = document.getElementById("userPanel");
 const userGreeting = document.getElementById("userGreeting");
